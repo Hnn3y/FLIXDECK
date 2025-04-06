03fc63ba78ce06a6528cc57b1a7791c5e59256a7
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-04-06 19:39:49 +0100

    build: Card.jsx

---
+++ flixdeck/src/Card.jsx
@@ -0,0 +1,23 @@
+function Movie({movie}) {
+
+    function buttonClick(){
+        alert("clicked")
+    }
+    return <div className="movie-card">
+        <div className="movie-poster">
+            <img src={movie.url} alt={movie.title} />
+            <img/>
+
+            <div className="movie-overlay">
+                <button className="favorite" onClick={buttonClick}>.</button>
+            </div>
+        </div>
+
+        <div className="movie-info">
+            <h3>{movie.title}</h3>
+            <p>{movie.release}</p>
+        </div>
+    </div>
+} 
+
+export default Movie;
