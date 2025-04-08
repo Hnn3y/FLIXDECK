dc2a64433976196a83c7efb1c03e615e39924fe7
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-04-08 17:49:17 +0100

    build: Favorite.jsx

---
+++ flixdeck/src/pages/Favorite.jsx
@@ -0,0 +1,9 @@
+function Favorite() {
+    return 
+    <div className="favorites-empty">
+        <h2>No Favorites</h2>
+        <p>Sart adding movies to your favorites</p>
+    </div>
+}
+
+export default Favorite;
